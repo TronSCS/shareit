cb7dc1194536db2ac04edd47bc55388cdad25c87
tronsmartcontractspacebot <botgithub@tronsmartcontract.space>
2019-03-05 06:47:51 +0700

    Share code

---
+++ 1551743270821.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.2;
+
+contract freebie 
+{struct User{
+    uint id;
+    string name;
+    uint token;}
+
+mapping (address => bool)public user;
+
+event eventaddUser(
+uint indexed Userid);
+constructor()}
+
+function addUser(string_name) private{
+    require(!user{msg.sender});
+    require(uint id > 0;)
+    user[msg.sender] = true;
+    
+    user[_Userid].token +++++;}
